5d89816d2b33161369a23e34646986db1487c7a2
aashinsiby <aashin.csa1923@saintgits.org>
2024-01-04 11:15:08 +0530

    1.8

--- about.js
+++ about.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
- // Select the elements
- const loadingContainer = document.querySelector('.loading-container');
- const content = document.querySelector('.page-content');
+  // Select the elements
+  const loadingContainer = document.querySelector('.loading-container');
+  const content = document.querySelector('.page-content');
+  const mobContent = document.querySelector('.mobile-content');
 
- // Hide the content initially
- content.classList.add('hidden');
+  // Hide both contents initially
+  content.classList.add('hidden');
+  mobContent.classList.add('hidden');
 
- // Add a delay to simulate the loading animation
- setTimeout(() => {
-   // Hide the loading container (loading image) and show the content
-   loadingContainer.style.display = 'none';
-   content.classList.remove('hidden');
- }, 2000); // Change the delay as needed
+  // Add a delay to simulate the loading animation
+  setTimeout(() => {
+    loadingContainer.style.display = 'none'; // Hide loading container
+
+    if (window.innerWidth < 768) {
+      content.classList.add('hidden');
+      mobContent.classList.remove('hidden'); // Show mobile content
+    } else {
+      content.classList.remove('hidden'); // Show desktop content
+      mobContent.classList.add('hidden');
+    }
+  }, 2000); // Change the delay as needed
 });
